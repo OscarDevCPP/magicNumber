d50c01e9b8fc26aa0037f3c8839001bf823284a6
DeveloperOscar <oscarchh0799@gmail.com>
2024-01-24 21:00:31 -0500

    release game prepare

--- app.js
+++ app.js
@@ -1,76 +1,61 @@
-let numeroSecreto = 0;
-let intentos = 0;
-let listaNumerosSorteados = [];
-let numeroMaximo = 10;
+let secretNumber = 0;
+let intents = 0;
+let listSortNumbers = [];
+let maxNumber = 10;
 
-
-
-function asignarTextoElemento(elemento, texto) {
-    let elementoHTML = document.querySelector(elemento);
-    elementoHTML.innerHTML = texto;
-    return;
-}
-
-function verificarIntento() {
-    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
-    
-    if (numeroDeUsuario === numeroSecreto) {
-        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
-        document.getElementById('reiniciar').removeAttribute('disabled');
+function verifyIntent() {
+  let userNumber = parseInt(document.getElementById('valorUsuario').value);
+  if (!(userNumber === secretNumber)) {
+    document.getElementById('reiniciar').removeAttribute('disabled');
+    if (userNumber > secretNumber) {
+      assingTextElement('p', 'El número secreto es menor');
     } else {
-        //El usuario no acertó.
-        if (numeroDeUsuario > numeroSecreto) {
-            asignarTextoElemento('p','El número secreto es menor');
-        } else {
-            asignarTextoElemento('p','El número secreto es mayor');
-        }
-        intentos++;
-        limpiarCaja();
+      assingTextElement('p', 'El número secreto es mayor');
     }
-    return;
+    intents++;
+    cleanBox();
+  } else {
+    assingTextElement('p', `Acertaste el número en ${intents} ${(intents === 1) ? 'vez' : 'veces'}`);
+  }
+  return;
 }
 
-function limpiarCaja() {
-    document.querySelector('#valorUsuario').value = '';
+function cleanBox() {
+  document.querySelector('#valorUsuario').value = '';
 }
 
-function generarNumeroSecreto() {
-    let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
-
-    console.log(numeroGenerado);
-    console.log(listaNumerosSorteados);
-    //Si ya sorteamos todos los números
-    if (listaNumerosSorteados.length == numeroMaximo) {
-        asignarTextoElemento('p','Ya se sortearon todos los números posibles');
+function generateSecretNumber() {
+  let randomNumber = Math.floor(Math.random() * maxNumber) + 1;
+  if (listSortNumbers.length == maxNumber) {
+    assingTextElement('p', 'Ya se sortearon todos los números posibles');
+  } else {
+    if (listSortNumbers.includes(randomNumber)) {
+      return generateSecretNumber();
     } else {
-        //Si el numero generado está incluido en la lista 
-        if (listaNumerosSorteados.includes(numeroGenerado)) {
-            return generarNumeroSecreto();
-        } else {
-            listaNumerosSorteados.push(numeroGenerado);
-            return numeroGenerado;
-        }
+      listSortNumbers.push(randomNumber);
+      return randomNumber;
     }
+  }
 }
 
-function condicionesIniciales() {
-    asignarTextoElemento('h1','Juego del número secreto!');
-    asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
-    numeroSecreto = generarNumeroSecreto();
-    intentos = 1;
-    console.log(numeroSecreto);
+function initialConditions() {
+  assingTextElement('h1', 'Juego del número secreto!');
+  assingTextElement('p', `Indica un número del 1 al ${maxNumber}`);
+  secretNumber = generateSecretNumber();
+  intents = 1;
 }
 
-function reiniciarJuego() {
-    //limpiar caja
-    limpiarCaja();
-    //Indicar mensaje de intervalo de números 
-    //Generar el número aleatorio
-    //Inicializar el número intentos
-    condicionesIniciales();
-    //Deshabilitar el botón de nuevo juego
-    document.querySelector('#reiniciar').setAttribute('disabled','true');
-    
+function restartGame() {
+  cleanBox();
+  initialConditions();
+  document.querySelector('#reiniciar').setAttribute('disabled', 'true');
 }
 
-condicionesIniciales();
+function assingTextElement(element, text) {
+  let htmlElement = document.querySelector(element);
+  htmlElement.innerHTML = text;
+  return;
+}
+
+initialConditions();
+
